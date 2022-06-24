61d16245c95c69f0f6ad35ebf6c6801be09dbbe7
DESKTOP-S8ABVPR\82109 <hs183000@naver.com>
2022-06-24 17:04:53 +0900

    CSS in Javascript

--- app.js
+++ app.js
@@ -2,33 +2,14 @@ const h1=document.querySelector("div.hello:first-child h1");
 
 
 function handleClick(){
-    h1.style.color="blue";
-}
-function handlemoustenter(){
-    h1.innerText= "Mouse is here"
-}
-function handlemouseleave(){
-    h1.innerText="Mouse is gone"
-}
-function handleWindowResize(){
-    document.body.style.background="tomato"
-}
-function copystart(){
-    alert("hihihihi")
-}
-function handleWindowOffline(){
-    alert("SOS no WIFI");
-}
-function handleWindowOnline(){
-    alert("WIFI ON");
+    const currentcolor=h1.style.color
+    let newColor;
+  if(currentcolor==="tomato"){
+    newColor="blue";
+  }else{
+    newColor="tomato"
+  }
+  h1.style.color= newColor;
 }
 
 h1.addEventListener("click",handleClick);
-h1.addEventListener("mouseenter",handlemoustenter);
-h1.addEventListener("mouseleave",handlemouseleave);
-
-
-window.addEventListener("copy",copystart);
-window.addEventListener("resize",handleWindowResize);
-window.addEventListener("offline",handleWindowOffline);
-window.addEventListener("online",handleWindowOnline);
