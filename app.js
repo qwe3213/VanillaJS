c9691dca4d281f1adccac79da0976c5912c8114d
DESKTOP-S8ABVPR\82109 <hs183000@naver.com>
2022-06-25 16:11:12 +0900

    Events part Two

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const loginInput=document.querySelector("#login-form input");
-const loginForm= document.querySelector("#login-form")
+const loginForm= document.querySelector("#login-form");
+const link =document.querySelector("a");
+
 
 function onSubmit(event){
   event.preventDefault();
   console.log(loginInput.value); 
+
+}
+function handleLinkClick(event){
+   event.preventDefault();
+  console.log(event);
 }
 
 loginForm.addEventListener("submit",onSubmit);
+link.addEventListener("click",handleLinkClick)
