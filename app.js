906502d2a594b868410018a48960a00eade151c8
DESKTOP-S8ABVPR\82109 <hs183000@naver.com>
2022-06-25 15:04:04 +0900

    Input Values

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
-const h1=document.querySelector("div.hello:first-child h1");
+const loginInput=document.querySelector("#login-form input");
+const loginButton=document.querySelector("#login-form button");
 
-
-function handleClick(){
-    const activee ="active"
-   if(h1.className===activee){
-    h1.className="";
-   } else{
-    h1.className=activee
-   }
+function handleLoginBtn(){
+   console.log(loginInput.value);
+ 
 }
 
-h1.addEventListener("click",handleClick);
+loginButton.addEventListener("click",handleLoginBtn)
