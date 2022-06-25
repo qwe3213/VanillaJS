7cabd65c670704ca4538185a6950d31cd13f690d
DESKTOP-S8ABVPR\82109 <hs183000@naver.com>
2022-06-25 19:48:39 +0900

    Loading Username

--- app.js
+++ app.js
@@ -3,18 +3,33 @@ const loginForm= document.querySelector("#login-form");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME= "hidden" ;
+const USERNAME_KEY="username"
 function onSubmit(event){
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username=loginInput.value;
-  console.log(username);
-  greeting.innerText="Hello "+ username;
-  greeting.classList.remove(HIDDEN_CLASSNAME)
-  localStorage.setItem("username",username)
+  localStorage.setItem(USERNAME_KEY,username)
+  paintGreeting(username);
+  
+}
+function paintGreeting(username){
+   greeting.innerText= `Hello  ${username}`
+   greeting.classList.remove(HIDDEN_CLASSNAME)
+}
+
+
+const savedUsername= localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername=== null){
+   loginForm.classList.remove( HIDDEN_CLASSNAME)
+   loginForm.addEventListener("submit",onSubmit)
+
+}else{
+   paintGreeting(savedUsername)
 }
 function handleLinkClick(event){
    event.preventDefault();
   console.log(event);
 }
 
-loginForm.addEventListener("submit",onSubmit);
+
