6ebbc724a759d8474ebda8a2202e63469c53f497
DESKTOP-S8ABVPR\82109 <hs183000@naver.com>
2022-06-25 22:28:12 +0900

    PadStart

--- js/clock.js
+++ js/clock.js
@@ -2,8 +2,10 @@ const clock = document.querySelector("h2#clock");
 
 function getClock(){
    const date= new Date();
-
-   clock.innerText=(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`)
+    const Hours =String(date.getHours()).padStart(2,"0")
+    const Minutes =String(date.getMinutes()).padStart(2,"0")
+    const seconds =String(date.getSeconds()).padStart(2,"0")
+    clock.innerText=` ${Hours}:${Minutes}:${seconds}`
 }
 getClock()
-setInterval(getClock,1000)
+setInterval(getClock,1000); 
