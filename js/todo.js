eacc57e10393816566253714635ac6ab75b05d28
DESKTOP-S8ABVPR\82109 <hs183000@naver.com>
2022-06-27 15:12:50 +0900

    Adding ToDos

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,18 @@ const todoForm= document.getElementById("todo-form");
 const todoList= document.getElementById("todo-list");
 const todoInput=document.querySelector("#todo-form input")
 
+function paintTodo(newTodo){
+   const li = document.createElement("li");
+   const span =document.createElement("span");
+   li.appendChild(span);
+   span.innerText =newTodo;
+   todoList.appendChild(li);
+}
 function handleToDosubmit(event){
    event.preventDefault();
-   console.log(todoInput.value);
    const newTodo= todoInput.value;
    todoInput.value="";
+   paintTodo(newTodo)
 }
 
 todoForm.addEventListener("submit",handleToDosubmit)
