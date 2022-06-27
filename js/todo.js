876551de33c3821954cde1c72ad27b29f3d77587
DESKTOP-S8ABVPR\82109 <hs183000@naver.com>
2022-06-27 16:07:10 +0900

    Deleting To Dos

--- js/todo.js
+++ js/todo.js
@@ -1,11 +1,19 @@
 const todoForm= document.getElementById("todo-form");
 const todoList= document.getElementById("todo-list");
 const todoInput=document.querySelector("#todo-form input")
-
+ 
+function delteToDo(event){
+   const li =event.target.parentElement;
+   li.remove();
+}
 function paintTodo(newTodo){
    const li = document.createElement("li");
    const span =document.createElement("span");
+   const button=document.createElement("button");
+   button.innerText="X";
+   button.addEventListener("click",delteToDo)
    li.appendChild(span);
+   li.appendChild(button);
    span.innerText =newTodo;
    todoList.appendChild(li);
 }
